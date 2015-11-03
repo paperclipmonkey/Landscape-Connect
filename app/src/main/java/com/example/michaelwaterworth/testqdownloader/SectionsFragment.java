db2bc37057c1335518a968ea00165818c8dc507f
Michael Waterworth <mike@3equals.co.uk>
2015-11-03 08:56:58 +0000

    Save photo to file

--- app/src/main/java/com/example/michaelwaterworth/testqdownloader/SectionsFragment.java
+++ app/src/main/java/com/example/michaelwaterworth/testqdownloader/SectionsFragment.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -20,6 +22,11 @@ import android.widget.ViewFlipper;
 
 import com.google.gson.Gson;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by michaelwaterworth on 27/10/2015. Copyright Michael Waterworth
  */
@@ -30,6 +37,7 @@ public class SectionsFragment extends Fragment implements View.OnClickListener {
     protected ViewFlipper flipper;
     protected ViewGroup base;
     protected Questionnaire questionnaire;
+    protected Response response;
 
     public SectionsFragment() {
     }
@@ -77,6 +85,13 @@ public class SectionsFragment extends Fragment implements View.OnClickListener {
         //Build the UI
         buildSectionsView(objs2, (ViewGroup) base.findViewById(R.id.diary_page));
 
+
+        //See if there's already a semi-saved Response object
+
+        //else create a new one
+        response = new Response();
+
+
         return base;
     }
 
@@ -100,10 +115,41 @@ public class SectionsFragment extends Fragment implements View.OnClickListener {
     }
 
     public void buttonTakePhoto(View view) {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e("Log", ex.getLocalizedMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        response.photo = "file:" + image.getAbsolutePath();
+        return image;
     }
 
 
