c2d4975df860c048546178d3e7ecc7548f8e8924
Michael Waterworth <mike@3equals.co.uk>
2016-02-02 16:18:24 +0000

    Add AppIndexing

--- app/src/main/java/uk/co/threeequals/landscapeconnect/QuestionnairesActivity.java
+++ app/src/main/java/uk/co/threeequals/landscapeconnect/QuestionnairesActivity.java
@@ -14,6 +14,9 @@ import android.util.Log;
 import android.view.MenuItem;
 
 import com.alexbbb.uploadservice.UploadServiceBroadcastReceiver;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * First Activity shown on load
@@ -66,6 +69,11 @@ public class QuestionnairesActivity extends AppCompatActivity {
                 }
             };
     private DrawerLayout mDrawerLayout;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -123,6 +131,9 @@ public class QuestionnairesActivity extends AppCompatActivity {
         });
 
         switchFragment(QUESTIONNAIRES_FRAGMENT);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void switchFragment(int fragmentId) {
@@ -170,5 +181,45 @@ public class QuestionnairesActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         uploadReceiver.unregister(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Questionnaires Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://uk.co.threeequals.landscapeconnect/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Questionnaires Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://uk.co.threeequals.landscapeconnect/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
