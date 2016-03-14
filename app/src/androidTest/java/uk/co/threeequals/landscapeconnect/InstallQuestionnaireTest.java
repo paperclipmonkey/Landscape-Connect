c511d5c717ba401ee20831ebd7b0c08cc86c7afa
Michael Waterworth <mike@3equals.co.uk>
2016-03-14 10:03:34 +0000

    Update installQuestionnare test - still not working

--- app/src/androidTest/java/uk/co/threeequals/landscapeconnect/InstallQuestionnaireTest.java
+++ app/src/androidTest/java/uk/co/threeequals/landscapeconnect/InstallQuestionnaireTest.java
@@ -1,7 +1,141 @@
+/*
+ * Copyright 2015, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.co.threeequals.landscapeconnect;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.Until;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
 /**
- * Created by michaelwaterworth on 10/03/2016. Copyright Michael Waterworth
+ * Basic sample for unbundled UiAutomator.
  */
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
 public class InstallQuestionnaireTest {
+
+    private static final String PACKAGE
+            = "uk.co.threeequals.landscapeconnect";
+
+    private static final int LAUNCH_TIMEOUT = 5000;
+
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+
+    private static final String QUESTIONNAIRE_CODE = "UiAutomator";
+
+    private UiDevice mDevice;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+
+        // Launch the blueprint app
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);    // Clear out any previous instances
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(PACKAGE).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void checkPreconditions() {
+        assertThat(mDevice, notNullValue());
+    }
+
+    @Test
+    public void testInstallQuestionnaire_code() {
+        // Type text and then press the button.
+        mDevice.findObject(By.res(PACKAGE, "fab_menu"))
+                .click();
+
+        mDevice.waitForIdle();
+
+        mDevice.findObject(By.res(PACKAGE, "fab_link"))
+                .click();
+
+        mDevice.findObject(By.focused(true))
+                .setText(QUESTIONNAIRE_CODE);
+
+
+
+        // Verify the test is displayed in the Ui
+        UiObject2 changedText = mDevice
+                .wait(Until.findObject(By.res(PACKAGE, "textToBeChanged")),
+                        500 /* wait 500ms */);
+        assertThat(changedText.getText(), is(equalTo(STRING_TO_BE_TYPED)));
+    }
+
+    @Test
+    public void testChangeText_newActivity() {
+        // Type text and then press the button.
+        mDevice.findObject(By.res(PACKAGE, "activityChangeTextBtn"))
+                .click();
+        mDevice.findObject(By.res(PACKAGE, "editTextUserInput"))
+                .setText(STRING_TO_BE_TYPED);
+
+
+        // Verify the test is displayed in the Ui
+        UiObject2 changedText = mDevice
+                .wait(Until.findObject(By.res(PACKAGE, "show_text_view")),
+                        500 /* wait 500ms */);
+        assertThat(changedText.getText(), is(equalTo(STRING_TO_BE_TYPED)));
+    }
+
+    /**
+     * Uses package manager to find the package name of the device launcher. Usually this package
+     * is "com.android.launcher" but can be different at times. This is a generic solution which
+     * works on all platforms.`
+     */
+    private String getLauncherPackageName() {
+        // Create launcher Intent
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+
+        // Use PackageManager to get the launcher package name
+        PackageManager pm = InstrumentationRegistry.getContext().getPackageManager();
+        ResolveInfo resolveInfo = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return resolveInfo.activityInfo.packageName;
+    }
 }
